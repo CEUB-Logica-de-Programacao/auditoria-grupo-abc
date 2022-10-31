bdd11bc1e088b8ed1fc181a1a7eed28d5c32d6f6
Enzo Henrique Ferreira Campos <98186034+EnzCampos@users.noreply.github.com>
2022-10-31 09:40:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ def etapa1(id):
     if len(idListado) == 4:
         index0 = int(idListado[0]+idListado[3])
         index1 = int(idListado[1]+idListado[2])
-
-        if (index0 + index1) <= 100:
-            soma = index0 + index1
+        soma = index0 + index1
             
     return soma
 
@@ -40,7 +38,7 @@ def etapa3(senha):
         return False
 
 if __name__ == "__main__":
-    if etapa1('1234') > 100:
+    if etapa1('7878') > 100:
             print('ID inválido')
             exit(1)
     if len(etapa2([1, 2,3,4,5,6,7,8,9])) > 0:
