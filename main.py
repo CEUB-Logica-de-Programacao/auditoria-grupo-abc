afd7972d384d8cad18771564c231fe61c5c849dd
Enzo Henrique Ferreira Campos <98186034+EnzCampos@users.noreply.github.com>
2022-10-31 09:18:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 def etapa1(id):
-    return 1000
+    if id.isnumeric():
+        idListado = sorted(list(id))
+        if len(idListado) == 4:
+            index0 = int(idListado[0]+idListado[3])
+            index1 = int(idListado[1]+idListado[2])
+            
+            if (index0 + index1) <= 100:
+                return print("Id é valido")
+            
+    print("Id é invalido")
 
 
-def etapa2(votos):
-    return []
+def etapa2(arr):
+    arrAscend = sorted(arr)
+    result = []
+    
+    if max(arr) < len(arrAscend):
+        for x in range(1, len(arrAscend)):
+            if (x + 1) not in arrAscend:
+                result.append(x+1)
+        
+    else:
+        for x in range(1, max(arr)):
+            if (x + 1) not in arrAscend:
+                result.append(x+1)
+                
+    return result
 
 
 def etapa3(senha):
-    return False
+    senhaSeparada = list(senha)
+    chars = list(Counter(senhaSeparada).values())
+    
+    if all(element == chars[0] for element in chars):
+        return print("Senha é válida")
+    else:
+        return print("Senha é inválida") 
 
 
 if __name__ == "__main__":
